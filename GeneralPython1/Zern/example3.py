871259aebc832cf5d6c709bad2cdb22e99914317
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-02-05 09:55:47 +0700

    update

---
+++ GeneralPython1/Zern/example3.py
@@ -0,0 +1,13 @@
+# print(True)
+# print(False)
+
+# Logic operational
+# 5>5 = False
+print(-5 > -2)
+print(-5 < -2)
+print(5 == 5) #check is it equal or not
+print(6 != 5) #check is it not equal or not
+
+x,y,z = 5,2,0
+print(x//y) #floor division
+print(x%y == 1) #modulus
