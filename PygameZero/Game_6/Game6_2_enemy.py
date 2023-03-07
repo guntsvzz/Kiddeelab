2e3c2dabb57d07cd92a9dc857d821b1cbcbc9573
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-03-07 20:45:13 +0700

    u0pdate

--- PygameZero/Game_6/Game6_2_enemy.py
+++ PygameZero/Game_6/Game6_2_enemy.py
@@ -10,26 +10,28 @@ tiles = {
 
 unlock = 0
 maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1, 2, 3, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1],
-    [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 4, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    #0  1  2  3  4  5  6  7  8  9  10 11 1
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #0
+    [1, 0, 0, 0, 1, 2, 3, 1, 1, 1, 1, 1, 1], #1
+    [1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1], #2
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], #3
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1], #4
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1], #5
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1], #6
+    [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 4, 1, 1], #7
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #8
 ]
 
 width,height = len(maze[0]),len(maze)
 TILE_SIZE = 70 #pixel of image
 WIDTH = TILE_SIZE * width
 HEIGHT = TILE_SIZE * height
-
+game_state = 'start'
 grassblock = Actor("grassblock", anchor=(0, 0), pos=(1 * TILE_SIZE, 1 * TILE_SIZE))
-enemy = Actor("blockermad", anchor=(-10, -10), pos=(3 * TILE_SIZE, 6 * TILE_SIZE))
-enemy.yv = -1
-enemy.xv = -1
+enemy_1 = Actor("blockermad", anchor=(-10, -10), pos=(7 * TILE_SIZE, 7 * TILE_SIZE))
+enemy_1.yv = -1
+enemy_2 = Actor("blockermad", anchor=(-10, -10), pos=(3 * TILE_SIZE, 6 * TILE_SIZE))
+enemy_2.yv = -1
 
 def draw():
     for row in range(len(maze)):
@@ -43,8 +45,9 @@ def draw():
                 screen.blit(tile, (x, y))
 
     grassblock.draw()
-    enemy.draw()
-
+    enemy_1.draw()
+    enemy_2.draw()
+    
 def on_key_down(key):
     global unlock
     #Moving Part
@@ -78,28 +81,27 @@ def on_key_down(key):
         unlock = unlock - 1
         maze[row][column] = 0 #path
 
-    enemyMoving()
+    enemyMoving(enemy_1)
+    enemyMoving(enemy_2)
 
-def enemyMoving():
+def enemyMoving(enemy):
     # enemy movement
     row = int(enemy.y / TILE_SIZE)
     column = int(enemy.x / TILE_SIZE)
-
     row = row + enemy.yv
-    column = column + enemy.xv
 
     tile = tiles[maze[row][column]]
-    if not tile == 'wall':
+    print(tile)
+    if tile == 'path':
         x = column * TILE_SIZE
         y = row * TILE_SIZE
+        print(f'row : {row}, column : {column}')
         animate(enemy, duration=0.1, pos=(x, y))
-    else:
-        rd = random.randint(1,2)
-        if rd == 1:
-            enemy.yv = enemy.yv * -1
-        elif rd == 2:
-            enemy.xv = enemy.xv * -1
-
+    else: #wall 
+        enemy.yv = enemy.yv * -1
+        print('Flip')
+        
+    #collision player
     if enemy.colliderect(grassblock):
         print("You died")
         exit()
